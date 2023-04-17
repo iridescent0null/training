255a98f246e8352fabf4d25f1ee756488264625f
Hiroumi Mitome <mitome@Force-of-Negation.local>
2023-04-18 07:22:16 +0900

    sophisticate core keeeper http eg memory value

--- araiCorekeeper.js
+++ araiCorekeeper.js
@@ -1,10 +1,18 @@
+const os = require('os');
 Bun.serve({
         port: 8082,
-        fetch: async (req) => {return new Response("<!DOCTYPE html><html><head><title>ARAI WORLD core keeper server</title><meta charset=\"UTF-8\"></head><body><h1>our core keeper server is running!</h1><div>key is:</div><div><i>key code will appear here</i><div></body></html>",
+        fetch: async (req) => {
+                const url = new URL(req.url);
+                if (url.pathname === "/status") {return new Response("<!DOCTYPE html><html><head><title>ARAI WORLD core keeper server</title><meta charset=\"UTF-8\"></head><body><h3>memory</h3><div>"+(1-(os.freemem()/os.totalmem()))*100+"%</div></body></html>",
+                {headers:{
+        "content-type":req.headers.get("content-type"),
+                },
+                });
+                }
+                return new Response("<!DOCTYPE html><html><head><title>ARAI WORLD core keeper server</title><meta charset=\"UTF-8\"></head><body><h1>our incredible core keeper server is running!</h1><div>key is:</div><div><i>hidden on git!</i><div><div>毎日AM 6:45再起動</div></body></html>",
                 {headers:{
         "content-type":req.headers.get("content-type"),
                 },
                 });
         },
-});
-
+});              
